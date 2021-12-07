fd5a37d665ae246c4d1e65d1dead9a7822a5b4e6
tamaron <tamaron1203@gmail.com>
2021-12-07 14:28:24 +0900

    The lexer is almost completed

--- src/ceylon_lexer/src/lib.rs
+++ src/ceylon_lexer/src/lib.rs
@@ -5,7 +5,6 @@ mod tests;
 
 use self::TokenKind::*;
 use cursor::Cursor;
-use std::str::Chars;
 
 #[derive(Debug)]
 pub struct Token {
@@ -101,7 +100,7 @@ pub enum LiteralKind {
     Str { terminated: bool },
 }
 
-pub fn is_whitespace(c: char) -> bool {
+fn is_whitespace(c: char) -> bool {
     matches!(
         c,
         '\u{0009}'   // \t
@@ -109,6 +108,14 @@ pub fn is_whitespace(c: char) -> bool {
         | '\u{000D}' // \r
         | '\u{0020}' // space
     )
+}
+
+fn is_id_start(c: char) -> bool {
+    c.is_ascii_alphanumeric() || c == '_'
+}
+
+fn is_id_continue(c: char) -> bool {
+    c.is_ascii_alphanumeric()
 }
 
 /// Parses the first token from the provided input string.
@@ -139,7 +146,61 @@ impl Cursor<'_> {
                 '/' => self.line_comment(),
                 _ => Slash,
             },
-            _ => panic!("Unexpected character: {}", first_char),
+            // Whitespace sequence.
+            c if is_whitespace(c) => self.whitespace(),
+
+            // Identifier (this should be checked after other variant that can
+            // start as identifier).
+            c if is_id_start(c) => self.ident(),
+
+            // Numeric literal.
+            // TODO:
+
+            // One-symbol tokens.
+            ';' => Semi,
+            ',' => Comma,
+            '.' => Dot,
+            '(' => OpenParen,
+            ')' => CloseParen,
+            '{' => OpenBrace,
+            '}' => CloseBrace,
+            '[' => OpenBracket,
+            ']' => CloseBracket,
+            '@' => At,
+            '#' => Pound,
+            '~' => Tilde,
+            '?' => Question,
+            ':' => Colon,
+            '$' => Dollar,
+            '=' => Eq,
+            '!' => Bang,
+            '<' => Lt,
+            '>' => Gt,
+            '-' => Minus,
+            '&' => And,
+            '|' => Or,
+            '+' => Plus,
+            '*' => Star,
+            '^' => Caret,
+            '%' => Percent,
+
+            // Character literal.
+            '\'' => {
+                let terminated = self.single_quoted_string();
+                Literal {
+                    kind: LiteralKind::Char { terminated },
+                }
+            }
+
+            // String literal.
+            '"' => {
+                let terminated = self.double_quoted_string();
+                Literal {
+                    kind: LiteralKind::Str { terminated },
+                }
+            }
+
+            _ => Unknown,
         };
         Token::new(token_kind, self.len_consumed())
     }
@@ -147,8 +208,78 @@ impl Cursor<'_> {
     fn line_comment(&mut self) -> TokenKind {
         self.bump(); // The second slash of "// comment"
         self.eat_while(|c| c != '\n');
-        self.bump(); // '\n'
         LineComment
+    }
+
+    fn whitespace(&mut self) -> TokenKind {
+        self.eat_while(is_whitespace);
+        Whitespace
+    }
+
+    fn ident(&mut self) -> TokenKind {
+        self.eat_while(is_id_continue);
+        Ident
+    }
+
+    fn single_quoted_string(&mut self) -> bool {
+        // Check if it's a one-symbol literal.
+        if self.second() == '\'' && self.first() != '\\' {
+            self.bump();
+            self.bump();
+            return true;
+        }
+
+        // Literal has more than one symbol.
+
+        // Parse until either quotes are terminated or error is detected.
+        loop {
+            match self.first() {
+                // Quotes are terminated, finish parsing.
+                '\'' => {
+                    self.bump();
+                    return true;
+                }
+                // Probably beginning of the comment, which we don't want to include
+                // to the error report.
+                '/' => break,
+                // Newline without following '\'' means unclosed quote, stop parsing.
+                '\n' if self.second() != '\'' => break,
+                // End of file, stop parsing.
+                _ if self.is_eof() => break,
+                // Escaped slash is considered one character, so bump twice.
+                '\\' => {
+                    self.bump();
+                    self.bump();
+                }
+                // Skip the character.
+                _ => {
+                    self.bump();
+                }
+            }
+        }
+        // String was not terminated.
+        false
+    }
+
+    /// Eats quoted string and returns true
+    /// if string is terminated.
+    /// Eats double-quoted string and returns true
+    /// if string is terminated.
+    fn double_quoted_string(&mut self) -> bool {
+        while let Some(c) = self.bump() {
+            match c {
+                '"' => {
+                    return true;
+                }
+                '\\' if self.first() == '\\' || self.first() == '"' => {
+                    // Bump again to skip escaped character.
+                    self.bump();
+                }
+                _ => (),
+            }
+        }
+        // End of file reached.
+        false
     }
 
     /// Eats symbols while predicate returns true or until the end of file is reached.
