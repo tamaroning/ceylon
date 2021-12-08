c49d7b3021978f879dd578f7168e58e523d6b3ff
tamaron <tamaron1203@gmail.com>
2021-12-08 17:27:39 +0900

    add lexer test

--- src/ceylon_lexer/src/tests.rs
+++ src/ceylon_lexer/src/tests.rs
@@ -13,35 +13,35 @@ fn test_all_tokens() {
     check_lexing(
         "// comment\n ;,.(){}[]@#~?:$=!<>-&|+*/^%",
         expect![[r#"
-            Token { kind: LineComment, len: 10 }
-            Token { kind: Whitespace, len: 2 }
-            Token { kind: Semi, len: 1 }
-            Token { kind: Comma, len: 1 }
-            Token { kind: Dot, len: 1 }
-            Token { kind: OpenParen, len: 1 }
-            Token { kind: CloseParen, len: 1 }
-            Token { kind: OpenBrace, len: 1 }
-            Token { kind: CloseBrace, len: 1 }
-            Token { kind: OpenBracket, len: 1 }
-            Token { kind: CloseBracket, len: 1 }
-            Token { kind: At, len: 1 }
-            Token { kind: Pound, len: 1 }
-            Token { kind: Tilde, len: 1 }
-            Token { kind: Question, len: 1 }
-            Token { kind: Colon, len: 1 }
-            Token { kind: Dollar, len: 1 }
-            Token { kind: Eq, len: 1 }
-            Token { kind: Bang, len: 1 }
-            Token { kind: Lt, len: 1 }
-            Token { kind: Gt, len: 1 }
-            Token { kind: Minus, len: 1 }
-            Token { kind: And, len: 1 }
-            Token { kind: Or, len: 1 }
-            Token { kind: Plus, len: 1 }
-            Token { kind: Star, len: 1 }
-            Token { kind: Slash, len: 1 }
-            Token { kind: Caret, len: 1 }
-            Token { kind: Percent, len: 1 }
+            Token { kind: LineComment, span: Span { start_pos: 0, len: 10 } }
+            Token { kind: Whitespace, span: Span { start_pos: 0, len: 2 } }
+            Token { kind: Semi, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Comma, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Dot, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: OpenParen, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: CloseParen, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: OpenBrace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: CloseBrace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: OpenBracket, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: CloseBracket, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: At, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Pound, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Tilde, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Question, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Colon, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Dollar, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Eq, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Bang, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Lt, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Gt, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Minus, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: And, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Or, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Plus, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Star, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Slash, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Caret, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Percent, span: Span { start_pos: 0, len: 1 } }
         "#]],
     )
 }
@@ -51,29 +51,33 @@ fn test_textual_literal() {
     check_lexing(
         "'a''b''\\n''\\t''\\r'\"Hello\"\"//\"",
         expect![[r#"
-            Token { kind: Literal { kind: Char { terminated: true } }, len: 3 }
-            Token { kind: Literal { kind: Char { terminated: true } }, len: 3 }
-            Token { kind: Literal { kind: Char { terminated: true } }, len: 4 }
-            Token { kind: Literal { kind: Char { terminated: true } }, len: 4 }
-            Token { kind: Literal { kind: Char { terminated: true } }, len: 4 }
-            Token { kind: Literal { kind: Str { terminated: true } }, len: 7 }
-            Token { kind: Literal { kind: Str { terminated: true } }, len: 4 }
+            Token { kind: Literal { kind: Char { terminated: true } }, span: Span { start_pos: 0, len: 3 } }
+            Token { kind: Literal { kind: Char { terminated: true } }, span: Span { start_pos: 0, len: 3 } }
+            Token { kind: Literal { kind: Char { terminated: true } }, span: Span { start_pos: 0, len: 4 } }
+            Token { kind: Literal { kind: Char { terminated: true } }, span: Span { start_pos: 0, len: 4 } }
+            Token { kind: Literal { kind: Char { terminated: true } }, span: Span { start_pos: 0, len: 4 } }
+            Token { kind: Literal { kind: Str { terminated: true } }, span: Span { start_pos: 0, len: 7 } }
+            Token { kind: Literal { kind: Str { terminated: true } }, span: Span { start_pos: 0, len: 4 } }
         "#]],
     )
 }
 
 #[test]
-fn test_unterinated_textual_literal() {
+fn test_unterminated_string_literal() {
     check_lexing(
         "\"",
         expect![[r#"
-            Token { kind: Literal { kind: Str { terminated: false } }, len: 1 }
+            Token { kind: Literal { kind: Str { terminated: false } }, span: Span { start_pos: 0, len: 1 } }
         "#]],
     );
+}
+
+#[test]
+fn test_unterminated_char_literal() {
     check_lexing(
         "\'",
         expect![[r#"
-            Token { kind: Literal { kind: Char { terminated: false } }, len: 1 }
+            Token { kind: Literal { kind: Char { terminated: false } }, span: Span { start_pos: 0, len: 1 } }
         "#]],
     );
 }
@@ -83,13 +87,13 @@ fn test_numerical_literal() {
     check_lexing(
         "0 1 638462071 3.141592",
         expect![[r#"
-            Token { kind: Literal { kind: Int }, len: 1 }
-            Token { kind: Whitespace, len: 1 }
-            Token { kind: Literal { kind: Int }, len: 1 }
-            Token { kind: Whitespace, len: 1 }
-            Token { kind: Literal { kind: Int }, len: 9 }
-            Token { kind: Whitespace, len: 1 }
-            Token { kind: Literal { kind: Float }, len: 8 }
+            Token { kind: Literal { kind: Int }, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Whitespace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Literal { kind: Int }, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Whitespace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Literal { kind: Int }, span: Span { start_pos: 0, len: 9 } }
+            Token { kind: Whitespace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Literal { kind: Float }, span: Span { start_pos: 0, len: 8 } }
         "#]],
     )
 }
@@ -99,17 +103,17 @@ fn test_method_access() {
     check_lexing(
         "10.foo() 2.72.foo()",
         expect![[r#"
-            Token { kind: Literal { kind: Int }, len: 2 }
-            Token { kind: Dot, len: 1 }
-            Token { kind: Ident, len: 3 }
-            Token { kind: OpenParen, len: 1 }
-            Token { kind: CloseParen, len: 1 }
-            Token { kind: Whitespace, len: 1 }
-            Token { kind: Literal { kind: Float }, len: 4 }
-            Token { kind: Dot, len: 1 }
-            Token { kind: Ident, len: 3 }
-            Token { kind: OpenParen, len: 1 }
-            Token { kind: CloseParen, len: 1 }
+            Token { kind: Literal { kind: Int }, span: Span { start_pos: 0, len: 2 } }
+            Token { kind: Dot, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Ident, span: Span { start_pos: 0, len: 3 } }
+            Token { kind: OpenParen, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: CloseParen, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Whitespace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Literal { kind: Float }, span: Span { start_pos: 0, len: 4 } }
+            Token { kind: Dot, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Ident, span: Span { start_pos: 0, len: 3 } }
+            Token { kind: OpenParen, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: CloseParen, span: Span { start_pos: 0, len: 1 } }
         "#]],
     )
 }
@@ -119,29 +123,66 @@ fn smoke_test() {
     check_lexing(
         "// my source file\nfunc main() -> Void { println(\"Hello.\"); }\n",
         expect![[r#"
-            Token { kind: LineComment, len: 17 }
-            Token { kind: Whitespace, len: 1 }
-            Token { kind: Ident, len: 4 }
-            Token { kind: Whitespace, len: 1 }
-            Token { kind: Ident, len: 4 }
-            Token { kind: OpenParen, len: 1 }
-            Token { kind: CloseParen, len: 1 }
-            Token { kind: Whitespace, len: 1 }
-            Token { kind: Minus, len: 1 }
-            Token { kind: Gt, len: 1 }
-            Token { kind: Whitespace, len: 1 }
-            Token { kind: Ident, len: 4 }
-            Token { kind: Whitespace, len: 1 }
-            Token { kind: OpenBrace, len: 1 }
-            Token { kind: Whitespace, len: 1 }
-            Token { kind: Ident, len: 7 }
-            Token { kind: OpenParen, len: 1 }
-            Token { kind: Literal { kind: Str { terminated: true } }, len: 8 }
-            Token { kind: CloseParen, len: 1 }
-            Token { kind: Semi, len: 1 }
-            Token { kind: Whitespace, len: 1 }
-            Token { kind: CloseBrace, len: 1 }
-            Token { kind: Whitespace, len: 1 }
+            Token { kind: LineComment, span: Span { start_pos: 0, len: 17 } }
+            Token { kind: Whitespace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Ident, span: Span { start_pos: 0, len: 4 } }
+            Token { kind: Whitespace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Ident, span: Span { start_pos: 0, len: 4 } }
+            Token { kind: OpenParen, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: CloseParen, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Whitespace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Minus, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Gt, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Whitespace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Ident, span: Span { start_pos: 0, len: 4 } }
+            Token { kind: Whitespace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: OpenBrace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Whitespace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Ident, span: Span { start_pos: 0, len: 7 } }
+            Token { kind: OpenParen, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Literal { kind: Str { terminated: true } }, span: Span { start_pos: 0, len: 8 } }
+            Token { kind: CloseParen, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Semi, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Whitespace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: CloseBrace, span: Span { start_pos: 0, len: 1 } }
+            Token { kind: Whitespace, span: Span { start_pos: 0, len: 1 } }
+        "#]],
+    )
+}
+
+fn check_string_reader(src: &str, expect: Expect) {
+    let mut tokens = Vec::new();
+    let mut reader = StringReader::new(src);
+    let mut token = reader.next_token();
+    while token.kind != TokenKind::Eof {
+        tokens.push(format!("{:?}\n", token));
+        token = reader.next_token();
+    }
+    tokens.push(format!("{:?}\n", token));
+    let actual: String = tokens.into_iter().collect();
+    expect.assert_eq(&actual);
+}
+
+#[test]
+fn test_string_reader() {
+    check_string_reader(
+        "// my source file\nfunc main() -> Void { println(\"Hello.\"); }\n",
+        expect![[r#"
+            Token { kind: Ident, span: Span { start_pos: 18, len: 4 } }
+            Token { kind: Ident, span: Span { start_pos: 23, len: 4 } }
+            Token { kind: OpenParen, span: Span { start_pos: 27, len: 1 } }
+            Token { kind: CloseParen, span: Span { start_pos: 28, len: 1 } }
+            Token { kind: Minus, span: Span { start_pos: 30, len: 1 } }
+            Token { kind: Gt, span: Span { start_pos: 31, len: 1 } }
+            Token { kind: Ident, span: Span { start_pos: 33, len: 4 } }
+            Token { kind: OpenBrace, span: Span { start_pos: 38, len: 1 } }
+            Token { kind: Ident, span: Span { start_pos: 40, len: 7 } }
+            Token { kind: OpenParen, span: Span { start_pos: 47, len: 1 } }
+            Token { kind: Literal { kind: Str { terminated: true } }, span: Span { start_pos: 48, len: 8 } }
+            Token { kind: CloseParen, span: Span { start_pos: 56, len: 1 } }
+            Token { kind: Semi, span: Span { start_pos: 57, len: 1 } }
+            Token { kind: CloseBrace, span: Span { start_pos: 59, len: 1 } }
+            Token { kind: Eof, span: Span { start_pos: 61, len: 0 } }
         "#]],
     )
 }
